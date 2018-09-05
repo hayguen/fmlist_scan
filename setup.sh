ded48cf84e75cd87f73b7e2c602e5368543557b1
hayati ayguen <h_ayguen@web.de>
2018-09-05 18:48:45 +0200

    move most file into src subdirectory
    Signed-off-by: hayati ayguen <h_ayguen@web.de>

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#export FMLIST_SCAN_USER="hayguen"   # default user "pi"
+#export FMLIST_SCAN_RASPI="0"       # default "1" if Raspberry Pi hardware
+#export FMLIST_SCAN_GPS_COORDS="48.885814 / 8.702681"
+
+pushd src &>/dev/null
+
+./all.sh "$@"
+
+popd &>/dev/null
