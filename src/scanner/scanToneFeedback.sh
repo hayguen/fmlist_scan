b98f0f1e912acf48e40abeaf894812033be9f13e
hayati ayguen <h_ayguen@web.de>
2018-10-30 22:37:14 +0000

    updated scanToneFeedback
    * play tone in background
    * easier discrimination of FM and DAB tone feedback
    
    Signed-off-by: hayati ayguen <h_ayguen@web.de>

--- src/scanner/scanToneFeedback.sh
+++ src/scanner/scanToneFeedback.sh
@@ -6,14 +6,32 @@ if [ -z "$1" ]; then
   exit 10
 fi
 
+
+case $1 in
+  fm|dab)
+    if [ $2 -ne 0 ]; then
+      # short for OK - found stations
+      PARA="200 100 300"
+    else
+      # long for FAIL - NO stations found
+      PARA="300 500 300"
+    fi
+    ;;
+  *)
+      PARA=""
+    ;;
+esac
+
 case $1 in
   fm)
     # FM: short short
-    pipwm 2000 10 0 1   50 100 50 300  &>/dev/null
+    pipwm 2000 10 0 1   50 100 50 ${PARA} &>/dev/null &
+    exit 0
     ;;
   dab)
     # DAB: short long
-    pipwm 2000 10 0 1   50 100 300 300  &>/dev/null
+    pipwm 2000 10 0 1   50 100 500 ${PARA} &>/dev/null &
+    exit 0
     ;;
   found)
     # DAB or FM found a carrier/station
@@ -22,32 +40,25 @@ case $1 in
     ;;
   welcome)
     # starting scanLoop
-    pipwm 2000 10 0 1   100 100 50 100 50 100   500 100 50 100  &>/dev/null
+    pipwm 2000 10 0 1   100 100 50 100 50 100   500 100 50 100  &>/dev/null &
     exit 0
     ;;
   saved)
     # saved scan results
-    pipwm 2000 10 0 1   50 100 50 100 50 100   150 100 150 100 150 100  &>/dev/null
+    pipwm 2000 10 0 1   50 100 50 100 50 100   150 100 150 100 150 100  &>/dev/null &
     exit 0
     ;;
   final)
     # closing/closed scanLoop
-    pipwm 2000 10 0 1   50 100 50 100 50 100   150 100 150 100 150 100   50 100 50 100 50 100  &>/dev/null
+    pipwm 2000 10 0 1   50 100 50 100 50 100   150 100 150 100 150 100   50 100 50 100 50 100  &>/dev/null &
     exit 0
     ;;
   error)
     # error, e.g. with RTL dongle
-    pipwm 2000 500 0 1  &>/dev/null
+    pipwm 2000 500 0 1  &>/dev/null &
     exit 0
     ;;
   *)
     ;;
 esac
 
-if [ $2 -ne 0 ]; then
-  # short for OK - found stations
-  pipwm 2000 10 0 1   100 300  &>/dev/null
-else
-  # long for FAIL - NO stations found
-  pipwm 2000 10 0 1   300 300  &>/dev/null
-fi
