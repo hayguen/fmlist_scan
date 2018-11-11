3710995325d12338deead8ae37b2d2a8feb07c66
hayati ayguen <h_ayguen@web.de>
2018-11-11 12:50:18 +0000

    fix/avoid parallel pipwm calls
    Signed-off-by: hayati ayguen <h_ayguen@web.de>

--- src/scanner/scanToneFeedback.sh
+++ src/scanner/scanToneFeedback.sh
@@ -6,6 +6,17 @@ if [ -z "$1" ]; then
   exit 10
 fi
 
+if [ -f /dev/shm/BeepPID ]; then
+  beepPid="$( cat /dev/shm/BeepPID )"
+  running="0"
+  #following is as "wait ${beepPid}" but also works for non-child processes
+  while [ -e /proc/${beepPid} ]; do running="1"; sleep 0.2; done
+  rm -f /dev/shm/BeepPID ]
+  if [ ${running} = "1" ]; then
+    sleep 0.8   # wait a bit more to create a small pause
+  fi
+fi
+
 
 case $1 in
   fm|dab)
@@ -26,36 +37,50 @@ case $1 in
   fm)
     # FM: short short
     pipwm 2000 10 0 1   50 100 50 ${PARA} &>/dev/null &
+    beepPid=$!
+    echo -n "${beepPid}" >/dev/shm/BeepPID
     exit 0
     ;;
   dab)
     # DAB: short long
     pipwm 2000 10 0 1   50 100 500 ${PARA} &>/dev/null &
+    beepPid=$!
+    echo -n "${beepPid}" >/dev/shm/BeepPID
     exit 0
     ;;
   found)
     # DAB or FM found a carrier/station
     pipwm 2000 10  &>/dev/null
+    beepPid=$!
+    echo -n "${beepPid}" >/dev/shm/BeepPID
     exit 0
     ;;
   welcome)
     # starting scanLoop
     pipwm 2000 10 0 1   100 100 50 100 50 100   500 100 50 100  &>/dev/null &
+    beepPid=$!
+    echo -n "${beepPid}" >/dev/shm/BeepPID
     exit 0
     ;;
   saved)
     # saved scan results
     pipwm 2000 10 0 1   50 100 50 100 50 100   150 100 150 100 150 100  &>/dev/null &
+    beepPid=$!
+    echo -n "${beepPid}" >/dev/shm/BeepPID
     exit 0
     ;;
   final)
     # closing/closed scanLoop
     pipwm 2000 10 0 1   50 100 50 100 50 100   150 100 150 100 150 100   50 100 50 100 50 100  &>/dev/null &
+    beepPid=$!
+    echo -n "${beepPid}" >/dev/shm/BeepPID
     exit 0
     ;;
   error)
     # error, e.g. with RTL dongle
     pipwm 2000 500 0 1  &>/dev/null &
+    beepPid=$!
+    echo -n "${beepPid}" >/dev/shm/BeepPID
     exit 0
     ;;
   *)
