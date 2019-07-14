a1f4a838a6541d515ad1086f1fd1b0031f335c75
hayati ayguen <h_ayguen@web.de>
2019-07-14 11:08:06 +0000

    update kal.sh for frequency calibration
    * added forwarding of additional options to kal
    * added option to choose scand band
    * check if anything was found in the scan band
    
    Signed-off-by: hayati ayguen <h_ayguen@web.de>

--- src/kal.sh
+++ src/kal.sh
@@ -12,6 +12,25 @@ fi
 
 ############################################
 
+if [ -z "$1" ]; then
+  echo "kal.sh [reuse] [GSM850|GSM-R|GSM900|EGSM|DCS|PCS] [<additional options to kal, e.g. -d 0>]"
+  echo "continuing in 3 sec with default parameters: no reuse and GSM900"
+  sleep 3
+fi
+
+OPT_REUSE=""
+if [ "$1" = "reuse" ]; then
+  OPT_REUSE="$1"
+  shift
+fi
+
+SCANBAND="GSM900"
+if [ "$1" = "GSM850" ] || [ "$1" = "GSM-R" ] || [ "$1" = "GSM900" ] || [ "$1" = "EGSM" ] || [ "$1" = "DCS" ] || [ "$1" = "PCS" ]; then
+  SCANBAND="$1"
+  shift
+fi
+
+
 echo "*** last PPM value ${FMLIST_SCAN_PPM}"
 echo "*** last calibration channel ${FMLIST_SCAN_KAL_CH}"
 
@@ -20,34 +39,39 @@ if [ ! -d "${RAMDIR}" ]; then
   mkdir "${RAMDIR}"
 fi
 
-if [ -z "${FMLIST_SCAN_KAL_CH}" ] || [ "$1" != "reuse" ]; then
+if [ -z "${FMLIST_SCAN_KAL_CH}" ] || [ "${OPT_REUSE}" != "reuse" ]; then
+
+  FMLIST_SCAN_KAL_CH=""
 
   echo "last calibration channel empty or no option 'reuse'"
-  echo "executing \"/usr/local/bin/kal -e ${FMLIST_SCAN_PPM} -s GSM900\"  to determine calibration channel"
-  /usr/local/bin/kal -e ${FMLIST_SCAN_PPM} -s GSM900 |tee "${RAMDIR}/kal.log"
+  echo "executing \"/usr/local/bin/kal $* -e ${FMLIST_SCAN_PPM} -s ${SCANBAND}\"  to determine calibration channel"
+  /usr/local/bin/kal $* -e ${FMLIST_SCAN_PPM} -s ${SCANBAND} |tee "${RAMDIR}/kal.log"
 
   echo "****"
 
-  cat "${RAMDIR}/kal.log" |grep chan |awk  '/.*/ { print $7, $2; }' |sort -n |tail -n 1 >"${RAMDIR}/kal-gsm900-max.log"
+  cat "${RAMDIR}/kal.log" |grep chan |awk  '/.*/ { print $7, $2; }' |sort -n |tail -n 1 >"${RAMDIR}/kal-${SCANBAND}-max.log"
 
-  maxchan=$(cat "${RAMDIR}/kal-gsm900-max.log" |awk '/.*/ { print $2; }')
-  maxpower=$(cat "${RAMDIR}/kal-gsm900-max.log" |awk '/.*/ { print $1; }')
-  echo "*** max GSM900 channel is $maxchan with power $maxpower"
+  maxchan=$(cat "${RAMDIR}/kal-${SCANBAND}-max.log" |awk '/.*/ { print $2; }')
+  maxpower=$(cat "${RAMDIR}/kal-${SCANBAND}-max.log" |awk '/.*/ { print $1; }')
+  echo "*** max ${SCANBAND} channel is ${maxchan} with power ${maxpower}"
   FMLIST_SCAN_KAL_CH="${maxchan}"
 
   if [ ! -z "${FMLIST_SCAN_KAL_CH}" ]; then
     sed -i '/FMLIST_SCAN_KAL_CH=/d' "$HOME/.config/fmlist_scan/config"
     echo "export FMLIST_SCAN_KAL_CH=\"${FMLIST_SCAN_KAL_CH}\"" >>"$HOME/.config/fmlist_scan/config"
+  else
+    echo "ERROR determining channel on band ${SCANBAND}. Verify that antenna is suitable for given band .. or try other band !"
+    exit 10
   fi
 else
   echo "re-using last calibration channel ${FMLIST_SCAN_KAL_CH}"
 fi
 
-echo "executing /usr/local/bin/kal -e ${FMLIST_SCAN_PPM} -c ${FMLIST_SCAN_KAL_CH}"
-/usr/local/bin/kal -e ${FMLIST_SCAN_PPM} -c ${FMLIST_SCAN_KAL_CH} |tee "${RAMDIR}/kal-channel.log"
+echo "executing /usr/local/bin/kal $* -e ${FMLIST_SCAN_PPM} -c ${FMLIST_SCAN_KAL_CH}"
+/usr/local/bin/kal $* -e ${FMLIST_SCAN_PPM} -c ${FMLIST_SCAN_KAL_CH} |tee "${RAMDIR}/kal-channel.log"
 
 NPPM=$(cat "${RAMDIR}/kal-channel.log" |grep "absolute error" |awk  '/.*/ { print $4; }')
-echo "*** new PPM value $NPPM"
+echo "*** new PPM value ${NPPM}"
 if [ ! -z "${NPPM}" ]; then
   sed -i '/FMLIST_SCAN_PPM=/d' "$HOME/.config/fmlist_scan/config"
   echo "export FMLIST_SCAN_PPM=\"${NPPM}\"  # ppm value of RTLSDR receiver" >>"$HOME/.config/fmlist_scan/config"
