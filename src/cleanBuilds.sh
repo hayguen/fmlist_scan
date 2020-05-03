f15854f18a6d40003a9d81d24f738426a95385b1
hayati ayguen <h_ayguen@web.de>
2020-05-03 16:05:40 +0000

    fix cleanBuilds.sh
    Signed-off-by: hayati ayguen <h_ayguen@web.de>

--- src/cleanBuilds.sh
+++ src/cleanBuilds.sh
@@ -4,8 +4,7 @@ rm -rf checkSpectrumForCarrier/build
 rm -rf prescanDAB/build
 rm -rf pipwm/build
 rm -rf pishutdown/build
+rm -rf git/hayguen/build_dab-cmdline_files
 rm -rf git/hayguen/build_dab-cmdline_rtlsdr
-rm -rf git/hayguen/build_dab-cmdline_raw
-rm -rf git/hayguen/build_dab-cmdline_wav
 rm -rf git/librtlsdr/build_librtlsdr
 rm -rf git/quiet/build_libcorrect
