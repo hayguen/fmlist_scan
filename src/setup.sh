17a833043b7db02177191909dbf993a7366b27e2
hayati ayguen <h_ayguen@web.de>
2020-06-11 12:09:42 +0000

    allow setup of multiple targets with single call
    added countdown .. before starting the setup.sh
    
    Signed-off-by: hayati ayguen <h_ayguen@web.de>

--- src/setup.sh
+++ src/setup.sh
@@ -64,135 +64,150 @@ echo "set export FMLIST_SCAN_RASPI=<1/0>   # default \"1\" if Raspberry Pi hardw
 echo ""
 
 
-if [ "$1" == "-h" ] || [ "$1" == "--h" ] || [ "$1" == "--help" ]; then
+if [ "$1" = "-h" ] || [ "$1" = "--h" ] || [ "$1" = "--help" ]; then
   exit 0
 fi
 
-if [ "$1" == "" ] && [ "$2" == "gui" ]; then
-  echo "will install/build ALL (including gui) - with empty 1st parameter"
-  sleep 5
-elif [ "$1" == "" ] && [ "$2" == "" ]; then
+if [ "$1" = "" ]; then
   echo "will install/build ALL (except gui) - without parameters"
-  sleep 5
+else
+  echo "will install/build selected options: $*"
 fi
+for C in $(seq 5 -1 1) ; do
+  echo -en "\r${C} secs to start .. press Ctrl-C to abort"
+  sleep 1
+done
+echo -e "\n\n"
 
-if [ "$1" == "syspre" ] || [ "$1" == "" ]; then
-  echo "installing system prerequisites"
-  . prereq_fmlist_scan
-fi
+while /bin/true; do
 
-if [ "$1" == "cron" ] || [ "$1" == "" ]; then
-  echo "installing crontab"
-  . prereq_crontab
-fi
-
-if [ "$1" == "fstab" ] || [ "$1" == "" ]; then
-  echo "installing fstab entry"
-  . prereq_fstab
-fi
-
-if [ "$1" == "files" ] || [ "$1" == "" ]; then
-  echo "installing scanner files"
-  . prereq_scan_files
-fi
-
-if [ "$1" == "conf" ] || [ "$1" == "" ]; then
-  echo "installing config files"
-  . prereq_config
-fi
-
-
-# gui software is not installed automatically
-if [ "$1" == "gui" ] || [ "$2" == "gui" ]; then
-  echo "installing gui software/tools"
-  . prereq_gui_software
-fi
-
-if [ "$1" == "pre" ] || [ "$1" == "" ]; then
-  echo "installing prerequisites"
-  . prereq_librtlsdr
-  . prereq_csdr
-  . prereq_liquid-dsp
-  . prereq_redsea
-  . prereq_dab-cmdline
-fi
-
-if [ "$1" == "rtl" ] || [ "$1" == "" ]; then
-  echo "building librtlsdr"
-  sudo -u ${FMLIST_SCAN_USER} bash -c "source build_librtlsdr"
-  . inst_librtlsdr
-fi
-
-if [ "$1" == "csdr" ] || [ "$1" == "" ]; then
-  echo "building csdr"
-  sudo -u ${FMLIST_SCAN_USER} bash -c "source build_csdr"
-  . inst_csdr
-fi
-
-if [ "$1" == "lfec" ] || [ "$1" == "" ]; then
-  echo "building libcorrect/libfec"
-  sudo -u ${FMLIST_SCAN_USER} bash -c "source build_libcorrect"
-  . inst_libcorrect
-fi
-
-if [ "$1" == "ldsp" ] || [ "$1" == "" ]; then
-  echo "building libliquid-dsp"
-  sudo -u ${FMLIST_SCAN_USER} bash -c "source build_liquid-dsp"
-  . inst_liquid-dsp
-fi
-
-if [ "$1" == "redsea" ] || [ "$1" == "" ]; then
-  echo "building redsea"
-  sudo -u ${FMLIST_SCAN_USER} bash -c "source build_redsea"
-  . inst_redsea
-fi
-
-if [ "$1" == "dabcmd" ] || [ "$1" == "" ]; then
-  echo "building dab-cmdline"
-  sudo -u ${FMLIST_SCAN_USER} bash -c "source build_dab-cmdline"
-  . inst_dab-cmdline
-fi
-
-if [ "$1" == "pipwm" ] || [ "$1" == "" ]; then
-  echo "building libwiringPi, pipwm"
-  sudo -u ${FMLIST_SCAN_USER} bash -c "source build_wiringpi"
-  . inst_wpi
-  sudo -u ${FMLIST_SCAN_USER} bash -c "source build_pipwm"
-  . setup_pipwm
-fi
-
-if [ "$1" == "pishutd" ] || [ "$1" == "" ]; then
-  echo "building libwiringPi, pishutdown"
-  sudo -u ${FMLIST_SCAN_USER} bash -c "source build_wiringpi"
-  . inst_wpi
-  sudo -u ${FMLIST_SCAN_USER} bash -c "source build_pishutdown"
-  . inst_pishutdown
-fi
-
-if [ "$1" == "chkspec" ] || [ "$1" == "" ]; then
-  echo "building libliquid-dsp"
-  sudo -u ${FMLIST_SCAN_USER} bash -c "source build_liquid-dsp"
-  . inst_liquid-dsp
-  echo "building checkSpectrumForCarrier"
-  sudo -u ${FMLIST_SCAN_USER} bash -c "source build_checkSpectrum"
-fi
-
-if [ "$1" == "pscan" ] || [ "$1" == "" ]; then
-  echo "building prescanDAB"
-  sudo -u ${FMLIST_SCAN_USER} bash -c "source build_prescanDAB"
-fi
-
-if [ "$1" == "kal" ] || [ "$1" == "" ]; then
-  echo "building kal"
-  sudo -u ${FMLIST_SCAN_USER} bash -c "source build_kal"
-  . inst_kal
-fi
-
-if [ ${FMLIST_SCAN_SETUP_LPIE} -ne 0 ]; then
-  if [ "$1" == "lpie" ] || [ "$1" == "" ]; then
-    echo "building lpie"
-    sudo -u ${FMLIST_SCAN_USER} bash -c "source build_lpie"
-    . inst_lpie
+  if [ ! -z "$1" ]; then
+    echo -e "\nstarting setup of option '$1'"
   fi
-fi
+
+  if [ "$1" = "syspre" ] || [ "$1" = "" ]; then
+    echo "installing system prerequisites"
+    . prereq_fmlist_scan
+  fi
+
+  if [ "$1" = "cron" ] || [ "$1" = "" ]; then
+    echo "installing crontab"
+    . prereq_crontab
+  fi
+
+  if [ "$1" = "fstab" ] || [ "$1" = "" ]; then
+    echo "installing fstab entry"
+    . prereq_fstab
+  fi
+
+  if [ "$1" = "files" ] || [ "$1" = "" ]; then
+    echo "installing scanner files"
+    . prereq_scan_files
+  fi
+
+  if [ "$1" = "conf" ] || [ "$1" = "" ]; then
+    echo "installing config files"
+    . prereq_config
+  fi
+
+  # gui software is not installed automatically
+  if [ "$1" = "gui" ]; then
+    echo "installing gui software/tools"
+    . prereq_gui_software
+  fi
+
+  if [ "$1" = "pre" ] || [ "$1" = "" ]; then
+    echo "installing prerequisites"
+    . prereq_librtlsdr
+    . prereq_csdr
+    . prereq_liquid-dsp
+    . prereq_redsea
+    . prereq_dab-cmdline
+  fi
+
+  if [ "$1" = "rtl" ] || [ "$1" = "" ]; then
+    echo "building librtlsdr"
+    sudo -u ${FMLIST_SCAN_USER} bash -c "source build_librtlsdr"
+    . inst_librtlsdr
+  fi
+
+  if [ "$1" = "csdr" ] || [ "$1" = "" ]; then
+    echo "building csdr"
+    sudo -u ${FMLIST_SCAN_USER} bash -c "source build_csdr"
+    . inst_csdr
+  fi
+
+  if [ "$1" = "lfec" ] || [ "$1" = "" ]; then
+    echo "building libcorrect/libfec"
+    sudo -u ${FMLIST_SCAN_USER} bash -c "source build_libcorrect"
+    . inst_libcorrect
+  fi
+
+  if [ "$1" = "ldsp" ] || [ "$1" = "" ]; then
+    echo "building libliquid-dsp"
+    sudo -u ${FMLIST_SCAN_USER} bash -c "source build_liquid-dsp"
+    . inst_liquid-dsp
+  fi
+
+  if [ "$1" = "redsea" ] || [ "$1" = "" ]; then
+    echo "building redsea"
+    sudo -u ${FMLIST_SCAN_USER} bash -c "source build_redsea"
+    . inst_redsea
+  fi
+
+  if [ "$1" = "dabcmd" ] || [ "$1" = "" ]; then
+    echo "building dab-cmdline"
+    sudo -u ${FMLIST_SCAN_USER} bash -c "source build_dab-cmdline"
+    . inst_dab-cmdline
+  fi
+
+  if [ "$1" = "pipwm" ] || [ "$1" = "" ]; then
+    echo "building libwiringPi, pipwm"
+    sudo -u ${FMLIST_SCAN_USER} bash -c "source build_wiringpi"
+    . inst_wpi
+    sudo -u ${FMLIST_SCAN_USER} bash -c "source build_pipwm"
+    . setup_pipwm
+  fi
+
+  if [ "$1" = "pishutd" ] || [ "$1" = "" ]; then
+    echo "building libwiringPi, pishutdown"
+    sudo -u ${FMLIST_SCAN_USER} bash -c "source build_wiringpi"
+    . inst_wpi
+    sudo -u ${FMLIST_SCAN_USER} bash -c "source build_pishutdown"
+    . inst_pishutdown
+  fi
+
+  if [ "$1" = "chkspec" ] || [ "$1" = "" ]; then
+    echo "building libliquid-dsp"
+    sudo -u ${FMLIST_SCAN_USER} bash -c "source build_liquid-dsp"
+    . inst_liquid-dsp
+    echo "building checkSpectrumForCarrier"
+    sudo -u ${FMLIST_SCAN_USER} bash -c "source build_checkSpectrum"
+  fi
+
+  if [ "$1" = "pscan" ] || [ "$1" = "" ]; then
+    echo "building prescanDAB"
+    sudo -u ${FMLIST_SCAN_USER} bash -c "source build_prescanDAB"
+  fi
+
+  if [ "$1" = "kal" ] || [ "$1" = "" ]; then
+    echo "building kal"
+    sudo -u ${FMLIST_SCAN_USER} bash -c "source build_kal"
+    . inst_kal
+  fi
+
+  if [ ${FMLIST_SCAN_SETUP_LPIE} -ne 0 ]; then
+    if [ "$1" = "lpie" ] || [ "$1" = "" ]; then
+      echo "building lpie"
+      sudo -u ${FMLIST_SCAN_USER} bash -c "source build_lpie"
+      . inst_lpie
+    fi
+  fi
+
+  shift
+  if [ "$1" = "" ]; then
+    break
+  fi
+
+done
 
