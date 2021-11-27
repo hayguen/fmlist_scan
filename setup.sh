85b2e25415f00b4e964ec67e824ed9b92e98da7c
hayati ayguen <h_ayguen@web.de>
2021-11-27 14:47:47 +0100

    update comments in setup.sh for installation on PC
    Signed-off-by: hayati ayguen <h_ayguen@web.de>

--- setup.sh
+++ setup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-#export FMLIST_SCAN_USER="hayguen"  # default OS user "pi"
+#export FMLIST_SCAN_USER="hayguen"  # default Linux OS user "pi" - with sudo rights
 #export FMLIST_SCAN_RASPI="0"       # default "1" if Raspberry Pi hardware
-#export FMLIST_SCAN_SETUP_GPS="0"   # default "1" to activate gpsd and cronjob for user
-#export FMLIST_SCAN_SETUP_LPIE="0"  # default "1" to install layerpie
+#export FMLIST_SCAN_SETUP_GPS="1"   # default "1" to activate gpsd and cronjob for user. set to "1" also for PC
+export FMLIST_SCAN_SETUP_LPIE="0"  # default "1" to install layerpie. set "0" on PC - but also Raspberry Pi
 
-#export FMLIST_SCAN_MOUNT="0"       # default "1" to setup FMLIST_SCAN_RESULT_DEV in /etc/fstab
+#export FMLIST_SCAN_MOUNT="0"       # default "1" to setup FMLIST_SCAN_RESULT_DEV in /etc/fstab. set "0" without USB memory stick
 #export FMLIST_SCAN_RESULT_DEV="/dev/sda1"
-#export FMLIST_SCAN_RESULT_DIR="/mnt/sda1"
+#export FMLIST_SCAN_RESULT_DIR="/mnt/sda1"  # where to mount the device .. and access the contents
+#export FMLIST_SCAN_RESULT_DIR="/home/fmlist/results"  # set this existing path, without USB memory stick
 
 #export FMLIST_USER=""              # optional: username (email) at https://www.fmlist.org/
 #export FMLIST_OM_ID=""             # optional: OM id at https://www.fmlist.org/
