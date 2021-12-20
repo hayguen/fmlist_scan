57c64750013a25bf85701a6b8e59c7c9a3ca2d16
hayati ayguen <h_ayguen@web.de>
2021-12-21 00:32:24 +0100

    remove layerPie from atx-knob config
    Signed-off-by: hayati ayguen <h_ayguen@web.de>

--- src/scanner/atx-knob.sh
+++ src/scanner/atx-knob.sh
@@ -51,7 +51,8 @@ else
   # 1x:    Info/Status Upload scan results
   # 2x:    Start/Stop  Switch UKW/DAB
   # 3x:    Shutdown    Switch Autostart
-  # 4x:                Switch Speaker?
+  # 4x:    Reboot      Switch LPIE
+  # 5x:                Switch Speaker?
 
   case "$1" in
     A1)
@@ -60,7 +61,8 @@ else
            echo "1x:  Info/Status Upload scan results" ; \
            echo "2x:  Start/Stop  Switch UKW/DAB" ; \
            echo "3x:  Shutdown    Switch Autostart" ; \
-           echo "4x:              Switch Speaker?" )
+           echo "4x:  Reboot      Switch LPIE" ; \
+           echo "5x:              Switch Speaker?" )
       echo "$T" >>${FMLIST_SCAN_RESULT_DIR}/fmlist_scanner/atx-knob.log
       ;;
     A2)
@@ -92,10 +94,13 @@ else
       echo "pressed B3: switch autostart" >>${FMLIST_SCAN_RESULT_DIR}/fmlist_scanner/atx-knob.log
       ;;
     B4)
-      echo "pressed B4: switch text-to-speech" >>${FMLIST_SCAN_RESULT_DIR}/fmlist_scanner/atx-knob.log
+      echo "pressed B4: switch LPIE" >>${FMLIST_SCAN_RESULT_DIR}/fmlist_scanner/atx-knob.log
       ;;
     B5)
-      echo "pressed B5: no function" >>${FMLIST_SCAN_RESULT_DIR}/fmlist_scanner/atx-knob.log
+      echo "pressed B5: switch text-to-speech" >>${FMLIST_SCAN_RESULT_DIR}/fmlist_scanner/atx-knob.log
+      ;;
+    B6)
+      echo "pressed B6: no function" >>${FMLIST_SCAN_RESULT_DIR}/fmlist_scanner/atx-knob.log
       ;;
     *)
       echo "pressed $1 , but key is not handled in atx-knob.sh" >>${FMLIST_SCAN_RESULT_DIR}/fmlist_scanner/atx-knob.log
