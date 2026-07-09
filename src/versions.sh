90c551641950265ed86f8dac38df5ae9db3c06b2
andimik <andimik@yahoo.de>
2026-07-09 09:55:56 +0200

    shorten commit number and print commit name, html view changed

--- src/versions.sh
+++ src/versions.sh
@@ -15,62 +15,88 @@ if [ "$1" = "full" ]; then
   echo -e "\nos-release:"     ; (source /etc/os-release; echo "$PRETTY_NAME" )
   echo -e "\narchitecture:"   ; arch
 else
-  #GIT_DATE_FMT="--date=iso-strict"
-  GIT_DATE_FMT="--date=iso"
+  GIT_DATE_FMT="--date=short"
+  GIT_LOG_FMT="--pretty=format:%h %cd %<(55,trunc)%s"
 
-  d_fmlist_scan=$( cd .                   && git log ${GIT_DATE_FMT} -n 1 | egrep "^Date:" | sed 's/Date: //g' )
-  d_librtlsdr=$( cd git/hayguen/librtlsdr && git log ${GIT_DATE_FMT} -n 1 | egrep "^Date:" | sed 's/Date: //g' )
-  d_dabcmd=$( cd git/hayguen/dab-cmdline  && git log ${GIT_DATE_FMT} -n 1 | egrep "^Date:" | sed 's/Date: //g' )
-  d_eticmd=$( cd git/hayguen/eti-stuff    && git log ${GIT_DATE_FMT} -n 1 | egrep "^Date:" | sed 's/Date: //g' )
-#  d_csdrs=$( cd git/simonyiszk/csdr       && git log ${GIT_DATE_FMT} -n 1 | egrep "^Date:" | sed 's/Date: //g' )
-  d_csdrj=$( cd git/jketterl/csdr         && git log ${GIT_DATE_FMT} -n 1 | egrep "^Date:" | sed 's/Date: //g' )
-  d_redsea=$( cd git/windytan/redsea      && git log ${GIT_DATE_FMT} -n 1 | egrep "^Date:" | sed 's/Date: //g' )
-  d_ldsp=$( cd git/jgaeddert/liquid-dsp   && git log ${GIT_DATE_FMT} -n 1 | egrep "^Date:" | sed 's/Date: //g' )
-  d_libcorr=$( cd git/quiet/libcorrect    && git log ${GIT_DATE_FMT} -n 1 | egrep "^Date:" | sed 's/Date: //g' )
-  d_kalib=$( cd git/steve-m/kalibrate-rtl && git log ${GIT_DATE_FMT} -n 1 | egrep "^Date:" | sed 's/Date: //g' )
+  s_fmlist_scan=$( cd .                   && git log -n 1 ${GIT_DATE_FMT} "${GIT_LOG_FMT}" )
+  s_librtlsdr=$( cd git/hayguen/librtlsdr && git log -n 1 ${GIT_DATE_FMT} "${GIT_LOG_FMT}" )
+  s_dabcmd=$( cd git/hayguen/dab-cmdline  && git log -n 1 ${GIT_DATE_FMT} "${GIT_LOG_FMT}" )
+  s_eticmd=$( cd git/hayguen/eti-stuff    && git log -n 1 ${GIT_DATE_FMT} "${GIT_LOG_FMT}" )
+#  s_csdrs=$( cd git/simonyiszk/csdr       && git log -n 1 ${GIT_DATE_FMT} "${GIT_LOG_FMT}" )
+  s_csdrj=$( cd git/jketterl/csdr         && git log -n 1 ${GIT_DATE_FMT} "${GIT_LOG_FMT}" )
+  s_redsea=$( cd git/windytan/redsea      && git log -n 1 ${GIT_DATE_FMT} "${GIT_LOG_FMT}" )
+  s_ldsp=$( cd git/jgaeddert/liquid-dsp   && git log -n 1 ${GIT_DATE_FMT} "${GIT_LOG_FMT}" )
+  s_libcorr=$( cd git/quiet/libcorrect    && git log -n 1 ${GIT_DATE_FMT} "${GIT_LOG_FMT}" )
+  s_kalib=$( cd git/steve-m/kalibrate-rtl && git log -n 1 ${GIT_DATE_FMT} "${GIT_LOG_FMT}" )
+
   d_gpsd=$( sudo bash -l -c "which gpsd" )
-
-  c_fmlist_scan=$( cd .                   && git log ${GIT_DATE_FMT} -n 1 | egrep "^commit" | cut -d ' ' -f 2 )
-  c_librtlsdr=$( cd git/hayguen/librtlsdr && git log ${GIT_DATE_FMT} -n 1 | egrep "^commit" | cut -d ' ' -f 2 )
-  c_dabcmd=$( cd git/hayguen/dab-cmdline  && git log ${GIT_DATE_FMT} -n 1 | egrep "^commit" | cut -d ' ' -f 2 )
-  c_eticmd=$( cd git/hayguen/eti-stuff    && git log ${GIT_DATE_FMT} -n 1 | egrep "^commit" | cut -d ' ' -f 2 )
-#  c_csdrs=$( cd git/simonyiszk/csdr       && git log ${GIT_DATE_FMT} -n 1 | egrep "^commit" | cut -d ' ' -f 2 )
-  c_csdrj=$( cd git/jketterl/csdr         && git log ${GIT_DATE_FMT} -n 1 | egrep "^commit" | cut -d ' ' -f 2 )
-  c_redsea=$( cd git/windytan/redsea      && git log ${GIT_DATE_FMT} -n 1 | egrep "^commit" | cut -d ' ' -f 2 )
-  c_ldsp=$( cd git/jgaeddert/liquid-dsp   && git log ${GIT_DATE_FMT} -n 1 | egrep "^commit" | cut -d ' ' -f 2 )
-  c_libcorr=$( cd git/quiet/libcorrect    && git log ${GIT_DATE_FMT} -n 1 | egrep "^commit" | cut -d ' ' -f 2 )
-  c_kalib=$( cd git/steve-m/kalibrate-rtl && git log ${GIT_DATE_FMT} -n 1 | egrep "^commit" | cut -d ' ' -f 2 )
   c_gpsd=$( sudo bash -l -c "gpsd -V" )
   c_os_rel=$( (source /etc/os-release ; echo "$PRETTY_NAME" ) )
   c_arch=$( arch )
 
   if [ "$1" = "html" ]; then
-    echo "<table>"
-    echo "<tr><td>fmlist_scan</td><td>${c_fmlist_scan}</td><td>${d_fmlist_scan}</td></tr>"
-    echo "<tr><td>librtlsdr</td><td>${c_librtlsdr}</td><td>${d_librtlsdr}</td></tr>"
-    echo "<tr><td>dab-cmdline</td><td>${c_dabcmd}</td><td>${d_dabcmd}</td></tr>"
-    echo "<tr><td>eti-cmdline</td><td>${c_eticmd}</td><td>${d_eticmd}</td></tr>"
-#    echo "<tr><td>csdr</td><td>${c_csdrs}</td><td>${d_csdrs}</td></tr>"
-    echo "<tr><td>csdr++</td><td>${c_csdrj}</td><td>${d_csdrj}</td></tr>"
-    echo "<tr><td>redsea</td><td>${c_redsea}</td><td>${d_redsea}</td></tr>"
-    echo "<tr><td>libliquid-dsp</td><td>${c_ldsp}</td><td>${d_ldsp}</td></tr>"
-    echo "<tr><td>libcorrect</td><td>${c_libcorr}</td><td>${d_libcorr}</td></tr>"
-    echo "<tr><td>kalibrate-rtl</td><td>${c_kalib}</td><td>${d_kalib}</td></tr>"
-    echo "<tr><td>gpsd</td><td>${c_gpsd}</td><td>${d_gpsd}</td></tr>"
-    echo "<tr><td>os-release</td><td>${c_os_rel}</td><td></td></tr>"
-    echo "<tr><td>architecture</td><td>${c_arch}</td><td></td></tr>"
+    H_GIT_DATE_FMT="--date=short"
+    H_GIT_LOG_FMT="--pretty=format:%h|%cd|%s"
+
+    h_fmlist_scan=$( cd .                   && git log -n 1 ${H_GIT_DATE_FMT} "${H_GIT_LOG_FMT}" )
+    h_librtlsdr=$( cd git/hayguen/librtlsdr && git log -n 1 ${H_GIT_DATE_FMT} "${H_GIT_LOG_FMT}" )
+    h_dabcmd=$( cd git/hayguen/dab-cmdline  && git log -n 1 ${H_GIT_DATE_FMT} "${H_GIT_LOG_FMT}" )
+    h_eticmd=$( cd git/hayguen/eti-stuff    && git log -n 1 ${H_GIT_DATE_FMT} "${H_GIT_LOG_FMT}" )
+#    h_csdrs=$( cd git/simonyiszk/csdr       && git log -n 1 ${H_GIT_DATE_FMT} "${H_GIT_LOG_FMT}" )
+    h_csdrj=$( cd git/jketterl/csdr         && git log -n 1 ${H_GIT_DATE_FMT} "${H_GIT_LOG_FMT}" )
+    h_redsea=$( cd git/windytan/redsea      && git log -n 1 ${H_GIT_DATE_FMT} "${H_GIT_LOG_FMT}" )
+    h_ldsp=$( cd git/jgaeddert/liquid-dsp   && git log -n 1 ${H_GIT_DATE_FMT} "${H_GIT_LOG_FMT}" )
+    h_libcorr=$( cd git/quiet/libcorrect    && git log -n 1 ${H_GIT_DATE_FMT} "${H_GIT_LOG_FMT}" )
+    h_kalib=$( cd git/steve-m/kalibrate-rtl && git log -n 1 ${H_GIT_DATE_FMT} "${H_GIT_LOG_FMT}" )
+
+    print_html_row() {
+      local repo="$1"
+      local raw="$2"
+      local hash rest date subj
+      hash="${raw%%|*}"
+      rest="${raw#*|}"
+      date="${rest%%|*}"
+      subj="${rest#*|}"
+      echo "<tr><td>${repo}</td><td>${hash}</td><td>${date}</td><td>${subj}</td></tr>"
+    }
+
+    echo "<style>"
+    echo ".versions-wrap{max-width:100%;overflow-x:auto;-webkit-overflow-scrolling:touch;}"
+    echo "table.versions{border-collapse:collapse;width:100%;min-width:720px;}"
+    echo "table.versions th,table.versions td{padding:4px 8px;border-bottom:1px solid #ddd;text-align:left;vertical-align:top;}"
+    echo "table.versions td:nth-child(4){word-break:break-word;}"
+    echo "@media (max-width:700px){table.versions{font-size:12px;min-width:560px;} table.versions th,table.versions td{padding:3px 6px;}}"
+    echo "</style>"
+    echo "<div class=\"versions-wrap\">"
+    echo "<table class=\"versions\">"
+    echo "<tr><th>Repository</th><th>Commit</th><th>Date</th><th>Message</th></tr>"
+    print_html_row "fmlist_scan" "${h_fmlist_scan}"
+    print_html_row "librtlsdr" "${h_librtlsdr}"
+    print_html_row "dab-cmdline" "${h_dabcmd}"
+    print_html_row "eti-cmdline" "${h_eticmd}"
+#    print_html_row "csdr" "${h_csdrs}"
+    print_html_row "csdr++" "${h_csdrj}"
+    print_html_row "redsea" "${h_redsea}"
+    print_html_row "libliquid-dsp" "${h_ldsp}"
+    print_html_row "libcorrect" "${h_libcorr}"
+    print_html_row "kalibrate-rtl" "${h_kalib}"
+    echo "<tr><td>gpsd-version</td><td></td><td></td><td>${c_gpsd}</td></tr>"
+    echo "<tr><td>gpsd-path</td><td></td><td></td><td>${d_gpsd}</td></tr>"
+    echo "<tr><td>os-release</td><td></td><td></td><td>${c_os_rel}</td></tr>"
+    echo "<tr><td>architecture</td><td></td><td></td><td>${c_arch}</td></tr>"
     echo "</table>"
+    echo "</div>"
   else
-    echo "fmlist_scan    ${c_fmlist_scan} ${d_fmlist_scan}"
-    echo "librtlsdr      ${c_librtlsdr} ${d_librtlsdr}"
-    echo "dab-cmdline    ${c_dabcmd} ${d_dabcmd}"
-    echo "eti-cmdline    ${c_eticmd} ${d_eticmd}"
-#    echo "csdr           ${c_csdrs} ${d_csdrs}"
-    echo "csdr++         ${c_csdrj} ${d_csdrj}"
-    echo "redsea         ${c_redsea} ${d_redsea}"
-    echo "libliquid-dsp  ${c_ldsp} ${d_ldsp}"
-    echo "libcorrect     ${c_libcorr} ${d_libcorr}"
-    echo "kalibrate-rtl  ${c_kalib} ${d_kalib}"
+    echo "fmlist_scan    ${s_fmlist_scan}"
+    echo "librtlsdr      ${s_librtlsdr}"
+    echo "dab-cmdline    ${s_dabcmd}"
+    echo "eti-cmdline    ${s_eticmd}"
+#    echo "csdr           ${s_csdrs}"
+    echo "csdr++         ${s_csdrj}"
+    echo "redsea         ${s_redsea}"
+    echo "libliquid-dsp  ${s_ldsp}"
+    echo "libcorrect     ${s_libcorr}"
+    echo "kalibrate-rtl  ${s_kalib}"
     echo "gpsd           ${c_gpsd} ${d_gpsd}"
     echo "os-release     ${c_os_rel}"
     echo "architecture   ${c_arch}"
